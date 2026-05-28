1ded5412ba2ceebd9e125fdd9c4461e7c2489f84
DavidR <davidravicc@gmail.com>
2026-05-28 15:14:57 +0200

    added IconPic and SoundEffects for test

--- src/main/java/org/example/core/GuiJavaFx.java
+++ src/main/java/org/example/core/GuiJavaFx.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -20,6 +22,20 @@ public class GuiJavaFx extends Application {
 
 
         Scene scene = new Scene(label, screenBounds.getWidth()/1.5, screenBounds.getHeight()/1.5);
+
+        Image icon = new Image(getClass().getResourceAsStream("/images/Picture1.png"));
+        stage.getIcons().add(icon);
+
+
+
+
+        String soundPath = getClass()
+                .getResource("/sounds/soundStart.mp3")
+                .toExternalForm();
+
+        AudioClip clip = new AudioClip(soundPath);
+        clip.play();
+
 
 
         stage.setScene(scene);
